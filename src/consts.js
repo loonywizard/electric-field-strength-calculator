94c7501985e0e79b2f258cc7ec47f2187e6e841b
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-12-10 16:25:30 +0300

    Add SI_PREFIXES constants to consts.js file * Add prefixes Milli, Micro, Nano, Pico

---
+++ src/consts.js
@@ -0,0 +1,22 @@
+export const SI_PREFIXES = {
+  MILLI: {
+    value: 10 ** -3,
+    nameEn: 'm',
+    nameRu: 'м',
+  },
+  MICRO: {
+    value: 10 ** -3,
+    nameEn: 'µ',
+    nameRu: 'мк',
+  },
+  NANO: {
+    value: 10 ** -3,
+    nameEn: 'n',
+    nameRu: 'н',
+  },
+  PICO: {
+    value: 10 ** -3,
+    nameEn: 'p',
+    nameRu: 'п',
+  },
+};
