99f4dc72bc3127d5e551e7e6501767c8e4cd658d
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-12-10 20:15:49 +0300

    Set up webpack for scss :wrench:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss';
+
 import ChargesManager from './chargesManager';
 import ScreenSizeManager from './screenSizeManager';
 import DielectricConstantManager from './dielectricConstantManager';
