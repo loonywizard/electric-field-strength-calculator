e1a3c97a85ccaea7d619da180dff766f59767b0b
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-12-13 23:27:09 +0300

    Comment screenSizeManager :recycle:

--- src/screenSizeManager.js
+++ src/screenSizeManager.js
@@ -4,6 +4,10 @@ import Observer from './observer';
  * ScreenSizeManager allows us to detect changes of the screen size
  *
  * screenSize is the size of web page in pixels
+ *
+ * @function getScreenSize
+ *
+ * @extends Observer
  * */
 export default class ScreenSizeManager extends Observer {
   constructor() {
